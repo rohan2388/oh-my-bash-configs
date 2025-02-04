995452cfe416af2ab16a51acf38fe51f130dff59
Rohan Das <rohan@kraftdesignsmiths.com>
2025-02-04 23:35:53 +0530

    node aliases

---
+++ aliases/rdas.node.aliases.sh
@@ -0,0 +1,7 @@
+#! bash oh-my-bash.module
+#  ---------------------------------------------------------------------------
+
+alias pp='pnpm run'  # pnpm run
+alias pp='pnpx'      # pnpx
+alias nn='npm run'   # npm run
+alias nx='npx'       # npx
